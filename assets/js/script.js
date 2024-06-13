372467fbb7d10bec75fa2f896e89ba47062a6ad2
James Canterbury <jamescbury@proton.me>
2024-06-13 17:26:03 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    strings: ['interobjective autonomous worlds', 'confidential settlements', 'real world supply', 'authentic streaming', 'attested data'],
+    typeSpeed: 50,
+    backSpeed: 60,
+    loop: true
+  });
+});
