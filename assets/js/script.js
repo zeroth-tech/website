3e0ceb5ae2c94615263ce0c455583da309fcb55b
AnkMister <ankurusa1997@gmail.com>
2024-09-27 11:31:44 -0700

    formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,46 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   var typed = new Typed('#typed', {
     strings: ['interobjective autonomous worlds', 'confidential settlements', 'real world supply', 'authentic streaming', 'attested data'],
     typeSpeed: 50,
     backSpeed: 60,
     loop: true
   });
-});
 
-window.addEventListener('scroll', function() {
-  const header = document.querySelector('header');
   const logo = document.querySelector('.logo');
   const bigLogo = document.querySelector('.hero-content img');
-  const bigLogoOffset = bigLogo.offsetTop + bigLogo.offsetHeight;
+  let bigLogoOffset = bigLogo.offsetTop + bigLogo.offsetHeight;
 
-  if (window.pageYOffset > bigLogoOffset) {
-    logo.classList.add('show');
-  } else {
-    logo.classList.remove('show');
+  function updateLogoVisibility() {
+    if (window.pageYOffset > bigLogoOffset) {
+      logo.classList.add('visible');
+    } else {
+      logo.classList.remove('visible');
+    }
   }
 
-  const sections = document.querySelectorAll('section');
+  // Initial check for logo visibility
+  updateLogoVisibility();
 
-  sections.forEach((section, index) => {
-    if (index === 0) return;
+  window.addEventListener('scroll', function () {
+    updateLogoVisibility();
 
-    const sectionTop = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
-    const scrollPosition = window.pageYOffset;
+    const sections = document.querySelectorAll('section');
 
-    if (scrollPosition > sectionTop - window.innerHeight + sectionHeight / 2) {
-      section.classList.add('animate');
-    }
+    sections.forEach((section, index) => {
+      if (index === 0) return;
+
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const scrollPosition = window.pageYOffset;
+
+      if (scrollPosition > sectionTop - window.innerHeight + sectionHeight / 2) {
+        section.classList.add('animate');
+      }
+    });
+  });
+
+  // Update bigLogoOffset on window resize
+  window.addEventListener('resize', function () {
+    bigLogoOffset = bigLogo.offsetTop + bigLogo.offsetHeight;
   });
 });
