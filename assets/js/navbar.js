1147aca264f9306af96e8be558b6d289f185510c
Ethan Canterbury <ecanterbury@learnwillow.org>
2025-02-15 18:49:58 +0000

    Restored to '7b1edde96c3c9e95805bc6c2d14b0affb57ec951'
    Replit-Restored-To:7b1edde96c3c9e95805bc6c2d14b0affb57ec951

--- assets/js/navbar.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetch('/assets/components/navbar.html')
-    .then(response => response.text())
-    .then(data => {
-      document.body.insertAdjacentHTML('afterbegin', data);
-    });
-});
